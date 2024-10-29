8070867171458a03b41d6a7fddf34e4728b3dcf1
ibnu <ibnumuhthar9@gmail.com>
2024-10-29 09:19:34 +0530

    adjusted card space

--- src/Authentication/Authentication.tsx
+++ src/Authentication/Authentication.tsx
@@ -30,10 +30,13 @@ const AuthContainer = styled.div`
 background-color: #ffffff;
 box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
 width: 80%;
-height: 60%;
+display: flex;
+justify-content: center;
+flex-direction: column;
 border-radius: 18px;
 max-width: 40rem;
 min-height: 25rem;
+
 `
 
 
--- src/Authentication/Login.tsx
+++ src/Authentication/Login.tsx
@@ -24,6 +24,7 @@ input{
         font-style: italic;
         color: #aeaeae;
     }
+
 }
 
 `
